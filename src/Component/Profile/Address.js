2fa532ceb863cda7d04d19a2e7fd8e78045adc90
selim ben hlel <benhlel.slim@gmail.com>
2022-03-02 20:56:19 +0100

    first commit

---
+++ src/Component/Profile/Address.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Address() {
+    return (
+        <div>
+            <h2 className='blue'>My address: 6 rue de l'etudiant ennasr 2</h2>
+        </div>
+    )
+}
+
+export default Address
---
+++ src/Component/Profile/FullName.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function FullName() {
+    return (
+        <div>
+            <h1 className='red'>Selim ben hlel</h1>
+        </div>
+    )
+}
+
+export default FullName
